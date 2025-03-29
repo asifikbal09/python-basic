1897857873a89183edf50f078ad04b38ed672e6e
asifikbal09 <asif.ikbal662@gmail.com>
2025-03-29 23:01:07 +0600

    [DONE] Variable and Types of veriable.

---
+++ varTypes.py
@@ -0,0 +1,11 @@
+name= "Asif"
+age = 22
+hight = 5.5
+isBrown = True
+home = None
+
+print(type(name))
+print(type(age))
+print(type(hight))
+print(type(isBrown))
+print(type(home))
---
+++ variable.py
@@ -0,0 +1,6 @@
+name= "Asif"
+age = 22
+hight = 5.5
+isBrown = True
+
+print("My name is ", name,"I am ",age,"years old.", "My hight is", hight)
