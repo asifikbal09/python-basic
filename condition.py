3aaf7d449ada39081eee6b31b4cd4f449fda1a4a
asifikbal09 <asif.ikbal662@gmail.com>
2025-03-29 23:49:36 +0600

    [DONE] Condition.

---
+++ condition.py
@@ -0,0 +1,8 @@
+"""
+WAP to input 2 int numbers, a and b
+Print true if a is greater than or equal to b, If not print false.      
+"""
+a = int(input("Enter 1st number : "))
+b = int(input("Enter 2nd number : "))
+
+print(a>=b)
