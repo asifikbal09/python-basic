ccab1df2656eda384d2dc20c1e7df34242af082d
asifikbal09 <asif.ikbal662@gmail.com>
2025-03-29 23:41:38 +0600

    [DONE] Calculate the average of two float number.

---
+++ avg.py
@@ -0,0 +1,9 @@
+"""
+ Write a program to input 2 floating point number and print its average.
+"""
+
+num1 = float(input("Enter 1st float number ="))
+num2 = float(input("Enter 2nd float number ="))
+
+avg = (num1+num2)/2
+print("Average = ",avg)
